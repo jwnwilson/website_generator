0a0b80c416a467c5cfec41c6b34263213c504f0b
Noel Wilson <noelwilson@deloitte.co.uk>
2020-05-03 23:31:11 +0100

    Adding about me card to home page

--- src/client/src/cards/About/index.js
+++ src/client/src/cards/About/index.js
@@ -4,30 +4,30 @@ import PropTypes from 'prop-types';
 import './About.css';
 
 const About = (props) => {
-  const { module } = props.data;
+  const { data } = props;
   return (
     <section className="bg-primary text-white mb-0" id="about">
       <div className="container">
         <h2 className="text-center text-uppercase text-white">
-          {module.title}
+          {data.title}
         </h2>
         <hr className="star-light mb-5" />
         <div className="row">
           <div className="col-lg-4 ml-auto">
             <p className="lead">
-              {module.text_1}
+              {data.paragraph_1}
             </p>
           </div>
           <div className="col-lg-4 mr-auto">
             <p className="lead">
-              {module.text_2}
+              {data.paragraph_2}
             </p>
           </div>
         </div>
-        {module.cv_link
+        {data.cv_link
           && (
           <div className="text-center mt-4">
-            <a className="btn btn-xl btn-outline-light" target="_blank" href={module.cv_link}>
+            <a className="btn btn-xl btn-outline-light" target="_blank" href={data.cv_link}>
               <i className="fa fa-download mr-2" />
               CV Link
             </a>
