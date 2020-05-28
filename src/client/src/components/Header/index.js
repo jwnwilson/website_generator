adb64c12e3f0cbb5de8e18f2018e3145bcf64bd2
Noel Wilson <noelwilson@deloitte.co.uk>
2020-05-28 23:00:17 +0100

    adding comment to warn to update that code

--- src/client/src/components/Header/index.js
+++ src/client/src/components/Header/index.js
@@ -96,6 +96,8 @@ class Header extends React.Component {
             {link}
           </li>
         );
+        // Need to replace this with a non stupid solution
+        // We need to dynamically add target div ids to this list
         selectors.push(`card-section-${index+1}`);
       });
     }
