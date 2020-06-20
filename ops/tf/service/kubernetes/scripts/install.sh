1ef9d286a42891be86110162b2b21363c2ccac20
Noel Wilson <noelwilson@deloitte.co.uk>
2020-06-20 12:34:37 +0100

    Updating terraform bash scripts to automate kubernetes cluster setup

--- ops/tf/service/kubernetes/scripts/install.sh
+++ ops/tf/service/kubernetes/scripts/install.sh
@@ -3,8 +3,8 @@ set -e
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial-unstable main
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y docker.io
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+apt-get install -y kubelet kubeadm kubectl
