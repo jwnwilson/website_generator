9b0a04526607c11fba135690da6338239cd8b622
Noel Wilson <noelwilson@deloitte.co.uk>
2020-07-14 14:33:15 +0100

    Adding bootstrap css to javascript so it gets loaded in correct order in build

--- src/client/src/html.js
+++ src/client/src/html.js
@@ -12,12 +12,12 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <link 
+        {/* <link 
           rel="stylesheet" 
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossOrigin="anonymous"
-        />
+        /> */}
         <script
           src="https://code.jquery.com/jquery-3.4.1.min.js"
           crossOrigin="anonymous"
