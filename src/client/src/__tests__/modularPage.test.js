e582aa44cc81b8ffea83eebb1d856694caf16b43
Noel Wilson <noelwilson@deloitte.co.uk>
2020-08-15 16:18:08 +0100

    Updating tests to pass

--- src/client/src/__tests__/modularPage.test.js
+++ src/client/src/__tests__/modularPage.test.js
@@ -7,46 +7,39 @@ import renderer from 'react-test-renderer'
 
 import PageList from '../templates/modularPage';
 
-const testData = {page: {content: JSON.stringify({modules: []})}};
+const testData = {
+  site: {
+    siteMetadata: {
+      title: `Test Title`,
+    },
+  },
+  page: {
+    content: JSON.stringify(
+      {"title": "Home", "name": "Home", "url": "/", "slug": "home", "cards": [{ "__component": "card.welcome", "title": "Welcome", "name": "Welcome", "text": "Python - Django - Flask - Celery - Javascript - React - Kubernetes", "createdAt": "2020-07-12T10:17:47.279Z", "updatedAt": "2020-08-11T10:39:09.476Z", "__v": 0, "image": { "_id": "5f0ae34616f21d001e59fe83", "name": "jwn_hero_image", "alternativeText": "", "caption": "", "hash": "jwn_hero_image_b1197e95c3", "ext": ".jpeg", "mime": "image/jpeg", "size": 76.05, "width": 960, "height": 720, "url": "/uploads/jwn_hero_image_b1197e95c3.jpeg", "formats": { "thumbnail": { "hash": "thumbnail_jwn_hero_image_b1197e95c3", "ext": ".jpeg", "mime": "image/jpeg", "width": 208, "height": 156, "size": 6.96, "path": null, "url": "/uploads/thumbnail_jwn_hero_image_b1197e95c3.jpeg" }, "medium": { "hash": "medium_jwn_hero_image_b1197e95c3", "ext": ".jpeg", "mime": "image/jpeg", "width": 750, "height": 563, "size": 59.76, "path": null, "url": "/uploads/medium_jwn_hero_image_b1197e95c3.jpeg" }, "small": { "hash": "small_jwn_hero_image_b1197e95c3", "ext": ".jpeg", "mime": "image/jpeg", "width": 500, "height": 375, "size": 30.73, "path": null, "url": "/uploads/small_jwn_hero_image_b1197e95c3.jpeg" } }, "provider": "local", "related": [], "createdAt": "2020-07-12T10:17:42.477Z", "updatedAt": "2020-07-15T09:46:06.589Z", "__v": 0, "id": "5f0ae34616f21d001e59fe83" }, "id": "5f0ae34b16f21d001e59fe85" }, { "__component": "card.portfolio", "_id": "5f0f24150b7d309ef8275730", "title": "Portfolio", "name": "Projects", "projects": [{ "_id": "5f0f24150b7d309ef8275731", "title": "Safari Mixer", "description": "# Safari Mixer\\n\\n A Google voice experiment written in Node.js and React, available as a web app and also a google home app. Written end to end by myself as a short professional project at Rehab Studio.\\n\\n## Tech", "url": "https://safarimixer.beta.rehab/", "createdAt": "2020-07-15T15:43:17.513Z", "updatedAt": "2020-08-11T10:39:09.485Z", "__v": 0, "cover_image": { "_id": "5f0f24110b7d309ef827572f", "name": "safari-mixer-small", "alternativeText": "", "caption": "", "hash": "safari_mixer_small_c568e0cd3c", "ext": ".jpeg", "mime": "image/png", "size": 20.58, "width": 160, "height": 153, "url": "/uploads/safari_mixer_small_c568e0cd3c.jpeg", "formats": {}, "provider": "local", "related": [], "createdAt": "2020-07-15T15:43:13.748Z", "updatedAt": "2020-08-11T10:39:06.521Z", "__v": 0, "id": "5f0f24110b7d309ef827572f" }, "id": "5f0f24150b7d309ef8275731" }], "createdAt": "2020-07-15T15:43:17.503Z", "updatedAt": "2020-08-11T10:39:09.500Z", "__v": 1, "id": "5f0f24150b7d309ef8275730" }, { "__component": "card.contact-me", "_id": "5f1d78d3fc5ad0da568a71ef", "title": "Contact me", "send_url": "https://mail.jwnwilson-kube.co.uk/send", "createdAt": "2020-07-26T12:36:35.123Z", "updatedAt": "2020-08-11T10:39:09.461Z", "__v": 0, "id": "5f1d78d3fc5ad0da568a71ef" }], "createdAt": "2020-07-12T10:17:47.275Z", "updatedAt": "2020-08-11T10:39:09.537Z", "__v": 1, "deployed_at": "2020-07-13T14:00:00.016Z", "footer": { "_id": "5f0ef5b20b7d309ef827572d", "about_title": "About me", "about_me": "I'm a Full Stack Developer with VFX experience currently working in London I'm interested in web development, machine learning and data science!", "insta_url": "https://www.instagram.com/noelwilsonlon", "name": "Default", "location_title": "Location", "linkedin_url": "https://www.linkedin.com/in/noel-wilson-0a194225", "links_title": "Around the web", "github_url": "https://github.com/jwnwilson", "location": "Essex / London", "createdAt": "2020-07-15T12:25:22.477Z", "updatedAt": "2020-07-15T12:25:22.477Z", "__v": 0, "id": "5f0ef5b20b7d309ef827572d" }, "header": { "_id": "5f0ef5760b7d309ef827572c", "selectors": [{ "selector": "portfolio", "name": "Portfolio" }], "links": null, "title": "Noel Wilson", "name": "Default", "createdAt": "2020-07-15T12:24:22.541Z", "updatedAt": "2020-08-13T10:41:32.573Z", "__v": 0, "subtitle": "Full Stack Developer", "id": "5f0ef5760b7d309ef827572c" }, "id": "5f0ae34b16f21d001e59fe84" }
+    )
+  }
+};
 
 const setup = () => {
 }
 
 beforeEach(() => {
   // Use this for more complicated tests and logic
-  // useStaticQuery.mockImplementationOnce(({ render }) =>
-  //   render({
-  //     site: {
-  //       siteMetadata: {
-  //         title: `Default Starter`,
-  //       },
-  //     },
-  //   })
-  // )
-  useStaticQuery.mockReturnValue(
-    {
-      site: {
-        siteMetadata: {
-          title: `Test Title`,
-        },
-      },
-    }
-  );
+  useStaticQuery.mockReturnValue(testData);
 })
 
 describe('With Enzyme', () => {
-  it('App shows "Modular static website"', () => {
+  it('App shows "Noel Wilson"', () => {
     setup();
-
-    const pageList = render(<PageList data={testData}/>)
-    expect(pageList.find('h1').text()).toEqual('Test Title')
+    const pageList = render(<PageList data={testData} />)
+    expect(pageList.find('a').text()).toContain('Noel Wilson')
   })
 })
 
 describe('With Snapshot Testing', () => {
-  it('App shows "Modular static website"', () => {
+  it('App shows "Noel Wilson"', () => {
     setup();
-    const component = renderer.create(<PageList data={testData}/>)
+    const component = renderer.create(<PageList data={testData} />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
--- src/client/src/templates/modularPage.js
+++ src/client/src/templates/modularPage.js
@@ -14,7 +14,6 @@ export default class PageList extends React.Component {
 
   renderModule(moduleFileName, module, index, type, fallback = null) {
     const ModuleComponent = loadable(() => import(`../cards/${moduleFileName}`));
-    // console.log("Module data:" + JSON.stringify(module));
     return (
       <div key={index} data-module-index={index} data-module-type={type} id={"card-section-" + index}>
         <ModuleComponent data={module} fallback={fallback} />
@@ -39,19 +38,16 @@ export default class PageList extends React.Component {
     const content = JSON.parse(page.content);
     let components;
 
-    // console.log("page", page);
-    // console.log("content", content);
-    // console.log("content.cards", content.cards);
-
     if (content.cards) {
       components = content.cards.map((card, index) => {
         const card_title = card.title;
         const card_type = card.__component;
-        // const moduleFileName = capitalize(module_name);
+
         const { shouldLoadJavascript, moduleFileName } = this.getFromMap(card_type);
         // Get existing html from pre-rendered page if exists use as fallback for 
         // loadable component
         let htmlEl;
+
         // This should only run client side will try catch when building
         try {
           htmlEl = document.querySelector(`[data-module-index="${index.toString()}"]`);
